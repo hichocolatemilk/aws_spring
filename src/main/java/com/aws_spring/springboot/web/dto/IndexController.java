c09496a3f56e64ae208adb3c4b16bf77c11293ff
hichocolatemilk <111757770+hichocolatemilk@users.noreply.github.com>
2023-01-24 22:01:31 +0900

    게시글 posts/save 추가

--- src/main/java/com/aws_spring/springboot/web/dto/IndexController.java
+++ src/main/java/com/aws_spring/springboot/web/dto/IndexController.java
@@ -10,10 +10,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(){
-
+        
         return "index";
     }
-
+    
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
