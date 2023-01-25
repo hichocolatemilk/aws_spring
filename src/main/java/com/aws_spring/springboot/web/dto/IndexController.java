21a0846078dd54a6490f2d438e82bfc036848252
hichocolatemilk <111757770+hichocolatemilk@users.noreply.github.com>
2023-01-25 15:36:42 +0900

    게시글 수정 추가

--- src/main/java/com/aws_spring/springboot/web/dto/IndexController.java
+++ src/main/java/com/aws_spring/springboot/web/dto/IndexController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +21,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto postsResponseDto = postsService.findById(id);
+        model.addAttribute("post", postsResponseDto);
+
+        return "posts-update";
     }
 }
